c12f1f04376734465670c759675df5ead31c7274
jingyan wang <wayenvan@outlook.com>
2024-08-12 04:28:40 +0100

    okkkkkkk

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,8 +14,9 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lsp.neoconf" },
     -- import/override with your plugins
-    { import = "plugins"}
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.

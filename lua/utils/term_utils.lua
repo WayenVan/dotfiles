73347259414434454d92fc8daf6b06ecf901d9ca
Wang Jingyan <wayenvan@outlook.com>
2024-08-15 01:19:16 +0100

    okkkk

--- lua/utils/term_utils.lua
+++ lua/utils/term_utils.lua
@@ -3,7 +3,11 @@ local ret = {}
 local t = require("toggleterm.terminal")
 
 ---@param config TermCreateArgs
-function ret.creat_terminal(config)
+function ret.create_terminal(config)
+  -- set defualt values
+  if config.exit_on_close == nil then
+    config.exit_on_close = false
+  end
   t.Terminal:new(config):toggle()
 end
 

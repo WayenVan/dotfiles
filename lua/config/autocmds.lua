6345e2a604264ec2b3c8cec4f0f7bb083fcf70aa
jingyan wang <wayenvan@outlook.com>
2024-08-15 23:27:05 +0100

    okk

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,17 +4,27 @@
 --
 --
 --
---terminals
--- function _G.set_terminal_keymaps()
---   local opts = { buffer = 0 }
---   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
---   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
---   vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
---   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
---   vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
---   vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
---   vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
--- end
---
--- -- if you only want these mappings for toggle term use term://*toggleterm#* instead
--- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+-- Function to check for the existence of a file
+local function file_exists(name)
+  local f = io.open(name, "r")
+  if f then
+    f:close()
+  end
+  return f ~= nil
+end
+
+vim.api.nvim_create_augroup("AutoSourceNvimLua", { clear = true })
+
+-- Create an autocommand to run when entering a directory or opening a buffer
+vim.api.nvim_create_autocmd({ "BufEnter", "DirChanged" }, {
+  group = "AutoSourceNvimLua",
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local nvim_lua_file = cwd .. "/.nvim.lua"
+
+    if file_exists(nvim_lua_file) then
+      vim.cmd("luafile " .. nvim_lua_file)
+    end
+  end,
+})

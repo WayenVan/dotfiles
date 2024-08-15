b63a4b93483fc68ae4f6396817b4e500bf2b781d
Wang Jingyan <wayenvan@outlook.com>
2024-08-16 00:18:12 +0100

    add .nvim auto run

--- init.lua
+++ init.lua
@@ -3,4 +3,6 @@ if vim.g.vscode then
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
+  -- auto sourcing
+  require("utils.file").auto_sourcing()
 end
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,26 +5,12 @@
 --
 --
 
--- Function to check for the existence of a file
-local function file_exists(name)
-  local f = io.open(name, "r")
-  if f then
-    f:close()
-  end
-  return f ~= nil
-end
-
 vim.api.nvim_create_augroup("AutoSourceNvimLua", { clear = true })
 
--- Create an autocommand to run when entering a directory or opening a buffer
-vim.api.nvim_create_autocmd({ "BufEnter", "DirChanged" }, {
+-- Create an autocommand to run when entering a directory
+vim.api.nvim_create_autocmd({ "DirChanged" }, {
   group = "AutoSourceNvimLua",
   callback = function()
-    local cwd = vim.fn.getcwd()
-    local nvim_lua_file = cwd .. "/.nvim.lua"
-
-    if file_exists(nvim_lua_file) then
-      vim.cmd("luafile " .. nvim_lua_file)
-    end
+    require("utils.file").auto_sourcing()
   end,
 })

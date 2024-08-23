7e302b64def023d186f9449507c3989865dcc03f
Wang Jingyan <wayenvan@outlook.com>
2024-08-23 20:04:42 +0100

    modify highlithg of dap ui

--- lua/plugins/theme/tokyo_night.lua
+++ lua/plugins/theme/tokyo_night.lua
@@ -5,39 +5,56 @@ return {
     opts = {
       on_highlights = function(highlights, colors)
         -- need to be bold
-        -- bold cyan
         highlights["DapUIWinSelect"] = { link = "@markup.heading.1.markdown" }
-
-        -- bold gree
-        highlights["DapUIModifiedValue"] = { link = "@markup.heading.4.markdown" }
-        highlights["DapUIBreakpointsCurrentLine"] = { link = "@markup.heading.4.markdown" }
-        highlights["DapUICurrentFrameName"] = { link = "@markup.heading.4.markdown" }
+        -- bold
+        highlights["DapUIModifiedValue"] = { link = "@markup.list.markdown" }
+        -- bold
+        highlights["DapUIBreakpointsCurrentLine"] = { link = "@markup.list.markdown" }
 
         --cyan
         highlights["DapUIScope"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepOver"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepOverNC"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepInto"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepIntoNC"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepOut"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepOutNC"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepBack"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStepBackNC"] = { link = "@lsp.type.variable" }
-        highlights["DapUILineNumber"] = { link = "@lsp.type.variable" }
+        highlights["DapUILineNumber"] = { link = "Normal" }
         highlights["DapUIDecoration"] = { link = "@lsp.type.variable" }
-        highlights["DapUIBreakpointsPath"] = { link = "@lsp.type.variable" }
-        highlights["DapUIBreakpointsLine"] = { link = "@lsp.type.variable" }
-        highlights["DapUIStoppedThread"] = { link = "@lsp.type.variable" }
 
-        -- green
-        highlights["DapUIRestart"] = { link = "@attribute" }
-        highlights["DapUIRestartNC"] = { link = "@attribute" }
-        highlights["DapUIPlayPause"] = { link = "@attribute" }
-        highlights["DapUIPlayPauseNC"] = { link = "@attribute" }
+        -- breakpoint
+        highlights["DapUIBreakpointsPath"] = { link = "Directory" }
+        highlights["DapUIBreakpointsLine"] = { link = "Normal" }
+
+        -- thread
+        highlights["DapUICurrentFrameName"] = { link = "CursorLineNr" }
+        highlights["DapUIStoppedThread"] = { link = "Title" }
         highlights["DapUIThread"] = { link = "@attribute" }
+
+        --button
+        highlights["DapUIStepOverNC"] = { link = "@attribute" }
+        highlights["DapUIStepIntoNC"] = { link = "@attribute" }
+        highlights["DapUIStepOutNC"] = { link = "@attribute" }
+        highlights["DapUIStepBackNC"] = { link = "@attribute" }
+
+        -- button
+        highlights["DapUIStepOver"] = { link = "@attribute" }
+        highlights["DapUIStepInto"] = { link = "@attribute" }
+        highlights["DapUIStepOut"] = { link = "@attribute" }
+        highlights["DapUIStepBack"] = { link = "@attribute" }
+
+        highlights["DapUIRestart"] = { link = "@attribute" }
+        highlights["DapUIPlayPause"] = { link = "@attribute" }
+        highlights["DapUIRestartNC"] = { link = "@attribute" }
+        highlights["DapUIPlayPauseNC"] = { link = "@attribute" }
+
         highlights["DapUIBreakpointsInfo"] = { link = "@attribute" }
         highlights["DapUIWatchesValue"] = { link = "@attribute" }
+
+        -- not avialiable
+        highlights["DapUIUnavailable"] = { link = "NonText" }
+        highlights["DapUIUnavailableNC"] = { link = "NonText" }
+
+        -- type and source
+        highlights["DapUIType"] = { link = "Type" }
+        highlights["DapUISource"] = { link = "Directory" }
       end,
     },
   },
 }
+
+---@class OK

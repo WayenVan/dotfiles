fd4229efa49d1d36865055c438730e199d15080c
Wang Jingyan <wayenvan@outlook.com>
2024-08-23 01:28:46 +0100

    okk

--- lua/plugins/dap/python.lua
+++ lua/plugins/dap/python.lua
@@ -1,6 +1,20 @@
 -- set up python dap it was set by nvim-dap-python
 local dap = require("dap")
 
+-- modify configurations provided by  dap-python, so the cwd is always the root of the project
+for _, config in pairs(dap.configurations.python) do
+  local function cwd()
+    return vim.fn.getcwd()
+  end
+
+  if config.name == "Launch file" then
+    config.cwd = cwd
+  end
+  if config.name == "Launch file with arguments" then
+    config.cwd = cwd
+  end
+end
+
 -- dap.adapters.mypy = function(cb, config)
 --   cb({
 --     type = "executable",

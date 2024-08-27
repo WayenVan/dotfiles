c1ac7ad4ddde52b0aeae3a04dc02e449955995ef
Wang Jingyan <wayenvan@outlook.com>
2024-08-27 21:11:04 +0100

    ad wezterm settings

---
+++ .wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+local is_linux = function()
+  return wezterm.target_triple:find("linux") ~= nil
+end
+
+local is_darwin = function()
+  return wezterm.target_triple:find("darwin") ~= nil
+end
+
+config.font = wezterm.font("JetBrainsMonoNL NF")
+config.max_fps = 144
+
+if is_darwin() then
+  config.font_size = 16
+else
+  config.font_size = 11
+end
+
+local date = os.date("*t")
+if (tonumber(date.hour) >= 8) and (tonumber(date.hour) <= 19) then
+  config.color_scheme = "Everforest Light (Gogh)"
+else
+  config.color_scheme = "Everforest Dark (Gogh)"
+end
+
+return config

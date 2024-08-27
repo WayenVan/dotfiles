8461b0ee828c9358b733dad769fe73e12f2f6d1e
jingyan wang <wayenvan@outlook.com>
2024-08-27 16:31:10 +0000

    okkkkkkkk

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -5,7 +5,7 @@ return {
       strategy = {
         "toggleterm",
       },
-      templates = { "shell", "user.run_python" },
+      templates = { "shell", "user.run_python", "user.run_python_with_args" },
       default_detail = 3,
       task_list = {
         direction = "right",
@@ -17,15 +17,15 @@ return {
     },
     keys = function(_, keys)
       local new_keys = {
-        { "<leader>oo", "<cmd>OverseerToggle<cr>", desc = "Task list" },
-        { "<leader>oa", "<cmd>OverseerRun<cr>", desc = "Run task" },
-        { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Quick Action" },
-        { "<leader>oi", "<cmd>OverseerInfo<cr>", desc = "Overseer Info" },
+        { "<leader>oo", "<cmd>OverseerToggle<cr>",       desc = "Task list" },
+        { "<leader>oa", "<cmd>OverseerRun<cr>",          desc = "Run task" },
+        { "<leader>oq", "<cmd>OverseerQuickAction<cr>",  desc = "Quick Action" },
+        { "<leader>oi", "<cmd>OverseerInfo<cr>",         desc = "Overseer Info" },
         -- { "<leader>ob", "<cmd>OverseerBuild<cr>", desc = "Task builder" },
-        { "<leader>os", "<cmd>OverseerTaskAction<cr>", desc = "Task action" },
-        { "<leader>oc", "<cmd>OverseerClearCache<cr>", desc = "Clear cache" },
+        { "<leader>os", "<cmd>OverseerTaskAction<cr>",   desc = "Task action" },
+        { "<leader>oc", "<cmd>OverseerClearCache<cr>",   desc = "Clear cache" },
         { "<leader>od", "<cmd>OverseerDeleteBundle<cr>", desc = "Delete bundle" },
-        { "<leader>ol", "<cmd>OverseerLoadBundle<cr>", desc = "Delete bundle" },
+        { "<leader>ol", "<cmd>OverseerLoadBundle<cr>",   desc = "Delete bundle" },
       }
 
       return new_keys

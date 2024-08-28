f8e8fa716133ac5180df10536d62feb134802aa9
jingyan wang <wayenvan@outlook.com>
2024-08-28 16:07:32 +0100

    added some short modifications

--- tests/test.lua
+++ tests/test.lua
@@ -1,7 +1,6 @@
 -- print(vim.fn.expand("%:e"))
 require("overseer")
 -- print(vim.inspect(LazyVim.lualine.pretty_path()))
---
 
 print(require("nvim-navic").get_location())
 local wb = require("winbar")
--- tests/test.py
+++ tests/test.py
@@ -2,11 +2,8 @@ import numpy as np
 import math
 import time
 
-from numpy.lib.arraysetops import setxor1d
-
 for i in range(10):
     a = math.sqrt(i)
-    # time.sleep(60)
     print(a)
 
 

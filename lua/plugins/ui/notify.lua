97ac5fc35f2875c3e9c46eb74841e0761bab51c5
jingyan wang <wayenvan@outlook.com>
2024-08-29 15:34:58 +0100

    change timeout and fps of notify

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -1,12 +1,11 @@
 return {
   "rcarriga/nvim-notify",
   opts = {
-    timeout = 2000,
+    timeout = 2500,
     render = "compact",
     -- stages = "fade",
     -- stages = "fade_in_slide_out",
     stages = "static",
-    fps = 60,
     top_down = false,
   },
 }

40383b693d501f3bc04589080e4d10926c362e9e
jingyan wang <wayenvan@outlook.com>
2024-09-02 23:33:40 +0100

    chnage to powershell

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local misc = require("utils.misc")
+--
+-- shell setting
+if LazyVim.is_win() then
+  -- Set PowerShell as the default shell
+  vim.o.shell = "powershell"
+  vim.o.shellcmdflag = "-command"
+  vim.o.shellquote = '"'
+  vim.o.shellxquote = ""
+end
+
+-- local misc = require("utils.misc")
 
 -- sessiont setting
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"

d36881e5ac95ddec529a30f0e32d4d6a7e14aabb
jingyan wang <wayenvan@outlook.com>
2024-09-15 22:02:48 +0000

    datui fix

--- nvim/lua/plugins/ui/dap_ui.lua
+++ nvim/lua/plugins/ui/dap_ui.lua
@@ -11,8 +11,8 @@ return {
               id = "scopes",
               size = 0.25, -- Can be float or integer > 1
             },
-            { id = "watches", size = 0.25 },
-            { id = "stacks", size = 0.25 },
+            { id = "watches",     size = 0.25 },
+            { id = "stacks",      size = 0.25 },
             { id = "breakpoints", size = 0.25 },
           },
           size = 50,
@@ -20,7 +20,7 @@ return {
         },
         {
           elements = {
-            { id = "repl", size = 0.4 },
+            { id = "repl",    size = 0.4 },
             { id = "console", size = 0.6 },
             -- "repl",
             -- "console",
@@ -33,12 +33,8 @@ return {
     config = function(_, opts)
       local dap = require("dap")
       local dapui = require("dapui")
-      local tree = require("neo-tree.command")
       dapui.setup(opts)
       dap.listeners.after.event_initialized["dapui_config"] = function()
-        tree.execute({
-          action = "close",
-        })
         dapui.open({})
       end
       dap.listeners.before.event_terminated["dapui_config"] = function()

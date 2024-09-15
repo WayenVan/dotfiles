d423d0d01e1455e1a8e738629e8b30f9dc810a80
Jingyan Wang <wayenvan@outlook.com>
2024-09-15 03:52:58 +0100

    okk

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,11 +1,11 @@
 return {
   {
     "nvim-neotest/neotest",
-    opts = function(_, opts)
-      opts.consumers.overseer = nil
-      -- opts.adapters["neotest-python"] = {
-      --   runner = "pysd",
-      -- }
-    end,
+    -- opts = function(_, opts)
+    --   opts.consumers.overseer = nil
+    --   -- opts.adapters["neotest-python"] = {
+    --   --   runner = "pysd",
+    --   -- }
+    -- end,
   },
 }

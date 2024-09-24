b9f8ec7006176d383b94ea95699204d9bb0a0775
jingyan wang <wayenvan@outlook.com>
2024-09-24 16:55:04 +0100

    add venn drawing

---
+++ nvim/lua/plugins/venn.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "jbyuki/venn.nvim",
+    lazy = true,
+    keys = function(_, keys)
+      -- set
+      LazyVim.toggle.map(
+        "<leader>D",
+        LazyVim.toggle.wrap({
+          name = "toggle venn drawing",
+          get = function()
+            if vim.b.venn_enabled then
+              return true
+            else
+              return false
+            end
+          end,
+          set = function(state)
+            require("utils.draw").Toggle_venn()
+          end,
+        })
+      )
+      return keys
+    end,
+    cmd = { "VennToggle", "VBox" },
+  },
+}

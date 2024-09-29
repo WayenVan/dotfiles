bd9e60a2afd2e3d1bd57ee991f02a1d9b33323aa
Wang Jingyan <wayenvan@outlook.com>
2024-09-29 02:38:22 +0100

    change dropbar

--- nvim/lua/plugins/ui/dropbar.lua
+++ nvim/lua/plugins/ui/dropbar.lua
@@ -2,6 +2,13 @@ return {
   {
     "Bekaboo/dropbar.nvim",
     -- optional, but required for fuzzy finder support
+    opts = {
+      menu = {
+        win_configs = {
+          border = "single",
+        },
+      },
+    },
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
     },

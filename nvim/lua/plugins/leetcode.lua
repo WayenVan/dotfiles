3c5be4736b55cfc55032e6516e0f8eea38092aeb
Wang Jingyan <wayenvan@outlook.com>
2024-10-05 01:37:01 +0100

    add leedcode.lua

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,20 @@
+local leet_arg = "leetcode.nvim"
+return {
+  {
+    "kawre/leetcode.nvim",
+    lazy = leet_arg ~= vim.fn.argv()[1],
+    opts = { arg = leet_arg },
+    cmd = "Leet",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      -- "nvim-tree/nvim-web-devicons",
+    },
+  },
+}

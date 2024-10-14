dd60ac73ae89e9ce5136f9d1fa369bbf5d5f3d6b
jingyan wang <wayenvan@outlook.com>
2024-10-14 03:31:16 +0100

    change the menu

--- nvim/lua/plugins/ui/menu.lua
+++ nvim/lua/plugins/ui/menu.lua
@@ -13,11 +13,12 @@ return {
         desc = "open menu",
       },
       {
+        -- remove previous mouse mapping
         "<RightMouse>",
         function()
           -- only if buffer is a file buffer
           if vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "" then
-            require("menu").open("common", { border = true })
+            require("menu").open("common", { mouse = true, border = true })
           end
         end,
         desc = "open menu",

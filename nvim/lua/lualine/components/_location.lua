7536cacd3885bbefd2c5a06502fef5727a58492f
Wang Jingyan <wayenvan@outlook.com>
2024-10-16 22:11:11 +0100

    mofify lualine again

--- nvim/lua/lualine/components/_location.lua
+++ nvim/lua/lualine/components/_location.lua
@@ -1,13 +1,26 @@
 local M = require("lualine.component"):extend()
 local hl = require("lualine.highlight")
 
+local function progress()
+  local cur = vim.fn.line(".")
+  local total = vim.fn.line("$")
+  if cur == 1 then
+    return "Top"
+  elseif cur == total then
+    return "Bot"
+  else
+    return string.format("%2d%%%%", math.floor(cur / total * 100))
+  end
+end
+
 function M:update_status()
   local suffix = hl.get_mode_suffix()
   local line = vim.fn.line(".")
   local col = vim.fn.virtcol(".")
-  local icon = "%#lualine_a" .. suffix .. "# 󰍒 "
+  local progress = progress()
+  local proc = "%#lualine_a" .. suffix .. "# " .. progress .. " "
   local ret = string.format("[%d:%d] ", line, col)
-  ret = "%#lualine_b" .. suffix .. "#" .. ret .. icon .. "%#lualine_c_normal#"
+  ret = "%#lualine_b" .. suffix .. "#" .. ret .. proc .. "%#lualine_c_normal#"
   return ret
 end
 

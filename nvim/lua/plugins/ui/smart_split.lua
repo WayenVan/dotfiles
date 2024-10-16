14e1d3fbe8473eff70fea5c215182a45558967c1
jingyan wang <wayenvan@outlook.com>
2024-10-16 02:29:18 +0100

    change smart-splits  plugins

--- nvim/lua/plugins/ui/smart_split.lua
+++ nvim/lua/plugins/ui/smart_split.lua
@@ -3,7 +3,8 @@ return {
     "mrjones2014/smart-splits.nvim",
     event = "VeryLazy",
     opts = {
-      at_edge = function() end,
+      at_edge = "warp",
+      multiplexer_integration = false,
     },
     keys = {
       {
@@ -45,6 +46,34 @@ return {
       -- {"<C-w>H", function() require("smart-splits").swap_buf_left() end, desc = "Move Buffer Left"},
       -- {"<C-w>J", function() require("smart-splits").swap_buf_down() end, desc = "Move Buffer Down"},
       -- {"<C-w>K", function() require("smart-splits").swap_buf_up() end, desc = "Move Buffer Up"},
+      {
+        "<C-h>",
+        function()
+          require("smart-splits").move_cursor_left()
+        end,
+        desc = "move cursor left",
+      },
+      {
+        "<C-j>",
+        function()
+          require("smart-splits").move_cursor_down()
+        end,
+        desc = "move cursor down",
+      },
+      {
+        "<C-k>",
+        function()
+          require("smart-splits").move_cursor_up()
+        end,
+        desc = "move cursor up",
+      },
+      {
+        "<C-l>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        desc = "move cursor right",
+      },
     },
     -- {"<leader><leader>h", function() require("smart-splits").swap_buf_left() end, desc = "Swap Buffer Left"},
     -- {"<leader><leader>j", function() require("smart-splits").swap_buf_down() end, desc = "Swap Buffer Down"},

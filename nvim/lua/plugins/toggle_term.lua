d6273083978a250ccdafadbbf169e0f63f7c4769
jingyan wang <wayenvan@outlook.com>
2024-10-17 01:34:55 +0100

    change small part of toggle term

--- nvim/lua/plugins/toggle_term.lua
+++ nvim/lua/plugins/toggle_term.lua
@@ -119,6 +119,11 @@ return {
           return vim.o.columns * 0.4
         end
       end,
+      highlights = {
+        Normal = {
+          link = "Normal",
+        },
+      },
       open_mapping = [[<c-\>]],
       -- on_open = fun(t: Terminal), -- function to run when the terminal opens
       -- on_close = fun(t: Terminal), -- function to run when the terminal closes
@@ -144,31 +149,26 @@ return {
       hide_numbers = true, -- hide the number column in toggleterm buffers
       shade_filetypes = {},
       shade_terminals = true,
+      shading_factor = -30,
       -- shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
       start_in_insert = true,
-      insert_mappings = true,   -- whether or not the open mapping applies in insert mode
+      insert_mappings = true, -- whether or not the open mapping applies in insert mode
       terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
       persist_size = true,
       persist_mode = false,
       direction = "float" or "horizontal" or "vertical" or "window",
       -- direction = "float",
       close_on_exit = false, -- close the terminal window when the process exits
+
       -- shell = vim.o.shell, -- change the default shell
       -- This field is only relevant if direction is set to 'float'
-      -- float_opts = {
-      --   -- The border key is *almost* the same as 'nvim_open_win'
-      --   -- see :h nvim_open_win for details on borders however
-      --   -- the 'curved' border is a custom border type
-      --   -- not natively supported but implemented in this plugin.
-      --   border = 'single' or 'double' or 'shadow' or 'curved',
-      --   width = <value>,
-      --   height = <value>,
-      --   winblend = 3,
-      --   highlights = {
-      --     border = "Normal",
-      --     background = "Normal",
-      --   }
-      -- }
+      float_opts = {
+        -- The border key is *almost* the same as 'nvim_open_win'
+        -- see :h nvim_open_win for details on borders however
+        -- the 'curved' border is a custom border type
+        -- not natively supported but implemented in this plugin.
+        -- number = 1,
+      },
     },
   },
   {

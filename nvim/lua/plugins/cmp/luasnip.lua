367c687e806dcbc0835ecd7189f311096b6e2f66
jingyan wang <wayenvan@outlook.com>
2024-12-16 18:25:32 +0000

    feat(nvim): add fallback to the luasnipe keymap

--- nvim/lua/plugins/cmp/luasnip.lua
+++ nvim/lua/plugins/cmp/luasnip.lua
@@ -2,15 +2,27 @@ return {
   {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
+    -- enabled = false,
     config = function(_, opts)
       local ls = require("luasnip")
       -- --   ls.expand()
       -- -- end, { silent = true })
+
+      -- snip jump with fallback
       vim.keymap.set({ "i", "s" }, "<C-k>", function()
-        ls.jump(-1)
+        if ls.jumpable(-1) then
+          ls.jump(-1)
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-k>", true, true, true), "n", true)
+        end
       end, { silent = true, noremap = true, desc = "LusSnip jump to previous entry" })
+
       vim.keymap.set({ "i", "s" }, "<C-j>", function()
-        ls.jump(1)
+        if ls.jumpable(1) then
+          ls.jump(1)
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-j>", true, true, true), "n", true)
+        end
       end, { silent = true, noremap = true, desc = "LuaSnip jump to next entry" })
 
       ls.config.set_config(opts)

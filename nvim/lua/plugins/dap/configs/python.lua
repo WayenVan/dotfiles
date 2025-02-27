a8cedfda9cf445cd21717c35c13b5ef3d84d97fc
jingyan wang <wayenvan@outlook.com>
2025-02-27 15:58:35 +0000

    feat(nvim): change python dap configurations

--- nvim/lua/plugins/dap/configs/python.lua
+++ nvim/lua/plugins/dap/configs/python.lua
@@ -7,23 +7,22 @@ for _, config in pairs(dap.configurations.python) do
   local function cwd()
     return vim.fn.getcwd()
   end
-  if config.name == "Launch file" then
+  if config.name == "file" then
     config.cwd = cwd
     local extened_cfg = vim.deepcopy(config)
-    extened_cfg.name = "Launch file (Not just my code)"
+    extened_cfg.name = "file (Not just my code)"
     extened_cfg.justMyCode = false
     vim.list_extend(extra_cfgs, { extened_cfg })
   end
-  if config.name == "Launch file with arguments" then
+  if config.name == "file:args" then
     config.cwd = cwd
     local extened_cfg = vim.deepcopy(config)
-    extened_cfg.name = "Launch file with arguments (Not just my code)"
+    extened_cfg.name = "file:args (Not just my code)"
     extened_cfg.justMyCode = false
     vim.list_extend(extra_cfgs, { extened_cfg })
   end
 end
 vim.list_extend(dap.configurations.python, extra_cfgs)
-
 
 -- dap.adapters.mypy = function(cb, config)
 --   cb({

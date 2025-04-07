c9ad6604d850edd4c1e8d733960484ec4ee87c0a
jingyan wang <wayenvan@outlook.com>
2025-04-07 02:01:04 +0100

    feat(plugins): add keymap for tardis.nvim
    Add <leader>gT keymap to open Git Tardis plugin with description.

--- nvim/lua/plugins/tardis.lua
+++ nvim/lua/plugins/tardis.lua
@@ -2,6 +2,9 @@ return {
   {
     "fredeeb/tardis.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gT", "<cmd>Tardis<cr>", desc = "Git Tardis" },
+    },
     config = true,
   },
 }

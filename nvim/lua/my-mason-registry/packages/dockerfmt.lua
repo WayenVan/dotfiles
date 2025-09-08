19feb405b3182f8820efe64253da9f7105844e85
jingyan wang <wayenvan@outlook.com>
2025-09-08 03:28:45 +0100

    add mason-customized register

---
+++ nvim/lua/my-mason-registry/packages/dockerfmt.lua
@@ -0,0 +1,33 @@
+local Pkg = require("mason-core.package")
+local platform = require("mason-core.platform")
+
+return Pkg.new({
+  name = "dockerfmt",
+  desc = [[A custom CLI tool for testing mason local registry.]],
+  homepage = "https://github.com/reteps/dockerfmt",
+  categories = { Pkg.Cat.Formatter },
+  languages = { Pkg.Lang.docker },
+
+  install = function(ctx)
+    local asset = platform.is.win and "my-tool-windows.zip" or "dockerfmt-v0.3.7-linux-amd64.tar.gz"
+    local url = ("https://github.com/reteps/dockerfmt/releases/download/v0.3.7/%s"):format(asset)
+
+    -- 下载到当前目录
+    ctx.spawn.curl({ "-fL", url, "-o", asset })
+    ctx.spawn.tar({ "-xzf", asset })
+    ctx:link_bin("dockerfmt", "dockerfmt")
+
+    -- -- 解压（zip 或 tar.gz）
+    -- if asset:match("%.zip$") then
+    --   ctx.spawn.unpack({ asset }) -- 如果系统没有 unpack 命令，可替换成 `ctx.spawn.unzip { ... }`
+    -- else
+    --   -- 使用 tar 提取并去掉顶层目录（视包结构调整）
+    -- end
+
+    -- 标记来源（archive/http）
+    ctx.receipt:with_primary_source({
+      type = "github_release_file",
+      url = url,
+    })
+  end,
+})

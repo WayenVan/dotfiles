be0c284252382c76f0d058a64e9d08cf1ad568ef
jingyan wang <wayenvan@outlook.com>
2025-09-20 23:10:18 +0000

    fix mason

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -2,6 +2,6 @@ return {
   "mason-org/mason.nvim",
   opts = function(_, opts)
     opts.ensure_installed = vim.list_extend(opts.ensure_installed, { "tex-fmt", "ty", "pyrefly" })
-    opts.registries = { "lua:my-mason-registry", "github:mason-org/mason-registry" }
+    -- opts.registries = { "lua:my-mason-registry", "github:mason-org/mason-registry" }
   end,
 }

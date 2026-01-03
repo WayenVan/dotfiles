dbe8b7c3d96cad10c846e3ba25161e0d065b1ca9
Wang Jingyan <wayenvan@outlook.com>
2026-01-03 03:01:01 +0000

    change codecompanion setup

--- nvim/lua/plugins/ai/codecompanion.lua
+++ nvim/lua/plugins/ai/codecompanion.lua
@@ -80,6 +80,19 @@ return {
 
     config = function()
       require("codecompanion").setup({
+        display = {
+          action_palette = {
+            width = 95,
+            height = 10,
+            prompt = "Prompt ", -- Prompt used for interactive LLM calls
+            provider = "default", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+            opts = {
+              show_preset_actions = true, -- Show the preset actions in the action palette?
+              show_preset_prompts = true, -- Show the preset prompts in the action palette?
+              title = "CodeCompanion actions", -- The title of the action palette
+            },
+          },
+        },
         strategies = {
           chat = {
             adapter = "deepseek",

2bc81705d846646a169c42ab2c08ebaea911f9c4
jingyan wang <wayenvan@outlook.com>
2026-02-20 18:08:44 +0000

    fix bugs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -114,6 +114,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.keymap.set("t", "<C-h>", "<Cmd>wincmd h<CR>", { buffer = 0 }) -- Move left
     vim.keymap.set("t", "<C-j>", "<Cmd>wincmd j<CR>", { buffer = 0 }) -- Move down
     vim.keymap.set("t", "<C-l>", "<Cmd>wincmd l<CR>", { buffer = 0 }) -- Move right
+    vim.keymap.set("t", "<C-k>", "<Cmd>wincmd k<CR>", { buffer = 0 }) -- Move right
   end,
 })
 

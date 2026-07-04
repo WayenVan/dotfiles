4a8e63aa5d4ec6531921eb602196f90e38381300
Wang Jingyan <wayenvan@outlook.com>
2026-07-04 05:54:05 +0100

    update new config

---
+++ nvim/lua/plugins/jump/hop.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>j", "<cmd>HopLine<cr>", desc = "Hop line" },
+    },
+    opts = {
+      keys = "etovxqpdygfblzhckisuran",
+    },
+  },
+}

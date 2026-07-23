ab12d62b483dc366eb30f2aba1f10ffd087f2490
Wang Jingyan <wayenvan@outlook.com>
2026-07-23 07:13:51 +0100

    update siyuan mcps

--- mcps/siyuan.sh
+++ mcps/siyuan.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-uvx mcpo \
+# uvx mcpo \
+#   --host=0.0.0.0 \
+#   --port=7801 \
+#   --api-key="siyuan-wayenvan" \
+#   -- \
+#   npx -y @porkll/siyuan-mcp stdio \
+#   --token "$SIYUAN_TOKEN" \
+#   --baseUrl "http://127.0.0.1:6806"
+
+# uvx mcpo \
+#   --port 7801 \
+#   --server-type streamable-http \
+#   --header "{\"Authorization\":\"Bearer $SIYUAN_MCP_TOKEN\"}" \
+#   -- http://127.0.0.1:36806/mcp
+
+SIYUAN_API_URL=http://127.0.0.1:6806 \
+  uvx mcpo \
   --host=0.0.0.0 \
   --port=7801 \
   --api-key="siyuan-wayenvan" \
   -- \
-  npx -y @porkll/siyuan-mcp stdio \
-  --token "$SIYUAN_TOKEN" \
-  --baseUrl "http://127.0.0.1:6806"
+  node "$SIYUAN_MCP_PATH"

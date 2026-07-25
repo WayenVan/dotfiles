0c3c784242ae1da1bbd30669ec48ad38f5766ba1
Wang Jingyan <wayenvan@outlook.com>
2026-07-25 05:02:08 +0100

    update codex

--- nvim/lua/plugins/ai/sidekick.lua
+++ nvim/lua/plugins/ai/sidekick.lua
@@ -7,6 +7,14 @@ return {
         -- nes is also useful in normal mode
         { "<tab>", LazyVim.cmp.map({ "ai_nes" }, "<tab>"), mode = { "n" }, expr = true },
         { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+        {
+          "<c-.>",
+          function()
+            require("sidekick.cli").focus()
+          end,
+          desc = "Sidekick Focus",
+          mode = { "n", "t", "i", "x" },
+        },
         -- {
         --   "<leader>a/",
         --   function()

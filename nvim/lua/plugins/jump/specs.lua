b0a58ab5c7a29a473128e469660708b80ae02173
Wang Jingyan <wayenvan@outlook.com>
2026-08-05 17:29:34 +0100

    update specs

--- nvim/lua/plugins/jump/specs.lua
+++ nvim/lua/plugins/jump/specs.lua
@@ -2,16 +2,20 @@ return {
   {
     "cxwx/specs.nvim",
     -- lazy = false,
+
+    keys = {
+      { "<leader>;", "<cmd>lua require('specs').show_specs()<CR>", desc = "Show jump specs" },
+    },
     config = function(_, opts)
       require("specs").setup({
-        show_jumps = true,
+        show_jumps = false,
         min_jump = 30,
         popup = {
           delay_ms = 0, -- delay before popup displays
-          inc_ms = 10, -- time increments used for fade/resize effects
-          blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
-          width = 10,
-          winhl = "PMenu",
+          inc_ms = 8, -- time increments used for fade/resize effects
+          blend = 0, -- starting blend, between 0-100 (fully transparent), see :h winblend
+          width = 30,
+          winhl = "IncSearch",
           fader = require("specs").linear_fader,
           resizer = require("specs").shrink_resizer,
         },
